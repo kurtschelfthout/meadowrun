e4a3f6991f47984a947e159bd04cd618f918ab1c
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2022-07-04 10:12:57 +0100

    FIXUP: intermediate checkin

--- tests/benchmarks/ec2_startup_time.py
+++ tests/benchmarks/ec2_startup_time.py
@@ -5,6 +5,7 @@ import sys
 from pprint import pprint
 from statistics import median
 from time import monotonic
+from typing import Any
 from meadowrun.meadowrun_pb2 import ContainerAtTag, GitRepoCommit
 
 from meadowrun.run_job import (
@@ -31,8 +32,9 @@ async def main():
     async with await irp.get_instance_registrar() as instance_registrar:
         for i in range(nb_runs):
             start_time = monotonic()
-            pid1, host1 = await run_function(
-                remote_function,
+            _: Any = await run_function(
+                #     remote_function,
+                "example_package.example.example_runner",
                 AllocCloudInstance(1, 0.5, 80, irp.cloud_provider()),
                 Deployment(
                     interpreter=ContainerAtTag(
@@ -43,6 +45,7 @@ async def main():
                         commit="cb277fa1d35bfb775ed1613b639e6f5a7d2f5bb6",
                     ),
                 ),
+                args=["foo"],
             )
             times.append(monotonic() - start_time)
             await irp.clear_instance_registrar(instance_registrar)
